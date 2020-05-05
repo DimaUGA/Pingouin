9958b861fc3cc8851497ce97e1847d5aeb1006d5
Sevket73 <sevketsalkim73@gmail.com>
2020-05-05 17:39:48 +0200

    Add files via upload

--- Vue/Vue_Jeu.java
+++ Vue/Vue_Jeu.java
@@ -1,31 +1,54 @@
-package pingVue;
+package Vue;
 
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.GridLayout;
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JPanel;
+import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
-public class Vue_Jeu extends JPanel{
-	Graphics2D drawable;
-	private Object width;
-	private Object height;
-	JPanel contentPanel;
-	
-	void affiche_niveau() {
-		GridLayout grille1 = new GridLayout(2,2);
-		JButton b1 = new JButton("Bouton 1 ");
-		JButton b12 = new JButton("Bouton 12 ");
-		JButton b13 = new JButton("Bouton 13 ");
-		JButton b14 = new JButton("Bouton 14 ");
-		b1.setBounds(200,20,100,100);
-		//grille1.add(b1);
-		grille1.addLayoutComponent("b1", new JButton("Bouton 1 "));
-		grille1.addLayoutComponent("b1", new JButton("Bouton 1 "));
-		grille1.addLayoutComponent("b1", new JButton("Bouton 1 "));
-		contentPanel.setLayout(grille1);
+import Modele.Modele_plateau;
+
+public class Vue_Jeu {
+
+	private JFrame frame;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	/*
+	 * public static void main(String[] args) { EventQueue.invokeLater(new
+	 * Runnable() { public void run() { try { Vue_Jeu window = new Vue_Jeu();
+	 * window.frame.setVisible(true); } catch (Exception e) { e.printStackTrace(); }
+	 * } }); }
+	 */
+	/**
+	 * Create the application.
+	 */
+
+	public Vue_Jeu() {
+		// initialize();
 	}
 
-	
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	/*
+	 * private void initialize() { frame = new JFrame(); frame.setBounds(100, 100,
+	 * 450, 300); frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	 * frame.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+	 * 
+	 * table = new JTable(); table.setModel(new DefaultTableModel(new Object[][] { {
+	 * null, null }, { null, null }, }, new String[] { "New column", "New column"
+	 * })); frame.getContentPane().add(table); }
+	 */
+
+	public void Affiche_tableau(Modele_plateau plateau) {
+		for (int k = 0; k < 8; k++) {
+			for (int l = 0; l < 15; l++) {
+				System.out.print(plateau.getValeurCase(new Point(k, l)));
+			}
+			System.out.println();
+		}
+	}
+
 }
