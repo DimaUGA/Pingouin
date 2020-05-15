a7ae9f4e88bff0e4b76beeed8798622e36a44d39
Sweedix <sevketsalkim73@gmail.com>
2020-05-15 17:08:43 +0200

    ajout controleur ia non connecter

--- Controleur/Controleur_GO.java
+++ Controleur/Controleur_GO.java
@@ -1,7 +1,7 @@
 package Controleur;
 
 import Modele.Modele_Jeu;
-import Vue.Vue_Jeu;
+import Vue.Vue_Jeu_Entier;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,9 +10,9 @@ public class Controleur_GO implements ActionListener {
 
     public void actionPerformed(ActionEvent e){
         Modele_Jeu mj = new Modele_Jeu();
-        Vue_Jeu vj = new Vue_Jeu();
-        Controleur_Jeu cj = new Controleur_Jeu(mj, vj);
-        vj.getFenetre().setVisible(true);
+        Vue_Jeu_Entier vj = new Vue_Jeu_Entier();
+        new Controleur_Jeu(mj, vj);
+        vj.getFrame().setVisible(true);
     }
 
 }
