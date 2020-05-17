80e27fda9bee2511962603e88989a2dc0eb69092
Sweedix <sevketsalkim73@gmail.com>
2020-05-17 02:18:22 +0200

    fghfgh

---
+++ Lancement.java
@@ -0,0 +1,19 @@
+<<<<<<< Updated upstream:Launcher.java
+import Controleur.Controleur_Vue_Jeu;
+public class Launcher{
+
+	public static void main(String[] args)
+    {
+		new Controleur_Vue_Jeu();
+	}
+}
+=======
+
+public class Lancement{
+
+	public static void main(String[] args)
+    {
+		new Controleur_Vue_Jeu();
+	}
+}
+>>>>>>> Stashed changes:Lancement.java
